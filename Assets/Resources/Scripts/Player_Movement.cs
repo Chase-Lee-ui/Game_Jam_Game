2ee2abae24543fe60ab685b900e6b6451f9e904b
Chase Lee <72054052+Chase-Lee-ui@users.noreply.github.com>
2021-09-25 13:42:44 -1000

    Added more imports

---
+++ Assets/Resources/Scripts/Player_Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    public Vector2 speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(speed.x * inputX, speed.y * inputY, 0);
+
+        movement *= Time.deltaTime;
+
+        transform.Translate(movement);
+    }
+}
