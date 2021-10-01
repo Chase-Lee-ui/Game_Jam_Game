417f6eec36c29fff8df29ce8a80cfef967291ec6
Katherine Piniol <piniolk@hawaii.edu>
2021-10-01 12:29:31 -1000

    Created title screen with functionality
    Place holder title and play button leads to "base scene" for now.

---
+++ Assets/Resources/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+
+    public void PlayGame() {
+        SceneManager.LoadScene("Base Scene", LoadSceneMode.Single);
+    }
+
+    public void HowToPlay() {
+        SceneManager.LoadScene("How To Play", LoadSceneMode.Single);
+
+    }
+
+    public void BackToMenu() {
+        SceneManager.LoadScene("Title", LoadSceneMode.Single);
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
