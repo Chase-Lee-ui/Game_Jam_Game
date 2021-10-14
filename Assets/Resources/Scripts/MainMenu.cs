6002df2232ff663220b238d132e699c8201c5eb7
Katherine Piniol <piniolk@hawaii.edu>
2021-10-14 11:59:14 -1000

    Re-added art and scripts, and scenes to build settings

---
+++ Assets/Resources/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame() {
+        SceneManager.LoadScene("Level_1", LoadSceneMode.Single);
+    }
+    public void HowToPlay() {
+        SceneManager.LoadScene("How To Play", LoadSceneMode.Single);
+    }
+    public void QuitGame() {
+        Application.Quit();
+    }
+
+    public void TitleScreen() {
+        SceneManager.LoadScene("Title", LoadSceneMode.Single);
+    }
+}
